dca3aa71ccc90c506b9f24e87741db0f2ed61775
Madhura Borkar <madhura.borkar@bugsmirror.com>
2024-11-27 11:32:23 +0530

    changed the directory strycture

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "tailwindcss";
 const {nextui} = require("@nextui-org/react");
 const config: Config = {
   content: [
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
